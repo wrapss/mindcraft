edb14e3398f2d39e4b15634f362d076d2ac2c73a
wraps <me@matthieu.pw>
2024-11-20 17:39:55 +0100

    feat(local.js): update endpoint and add new parameters
    - Update the chat endpoint to '/api/chat'

--- src/models/local.js
+++ src/models/local.js
@@ -3,19 +3,22 @@ import { strictFormat } from '../utils/text.js';
 export class Local {
     constructor(model_name, url) {
         this.model_name = model_name;
-        this.url = url || 'http://127.0.0.1:11434';
+        this.url = url || 'http://127.0.0.1:11442';
         this.chat_endpoint = '/api/chat';
         this.embedding_endpoint = '/api/embeddings';
+        this.context_length = 8192;
+        this.num_predict = 2048;
+        this.temperature = 0.3;
     }
 
     async sendRequest(turns, systemMessage) {
         let model = this.model_name || 'llama3';
         let messages = strictFormat(turns);
-        messages.unshift({role: 'system', content: systemMessage});
+        messages.unshift({ role: 'system', content: systemMessage });
         let res = null;
         try {
             console.log(`Awaiting local response... (model: ${model})`)
-            res = await this.send(this.chat_endpoint, {model: model, messages: messages, stream: false});
+            res = await this.send(this.chat_endpoint, { model: model, messages: messages, stream: false, temperature: this.temperature, options: { num_ctx: this.context_length, num_predict: this.num_predict } });
             if (res)
                 res = res['message']['content'];
         }
@@ -33,7 +36,7 @@ export class Local {
 
     async embed(text) {
         let model = this.model_name || 'nomic-embed-text';
-        let body = {model: model, prompt: text};
+        let body = { model: model, prompt: text };
         let res = await this.send(this.embedding_endpoint, body);
         return res['embedding']
     }
@@ -42,7 +45,8 @@ export class Local {
         const url = new URL(endpoint, this.url);
         let method = 'POST';
         let headers = new Headers();
-        const request = new Request(url, {method, headers, body: JSON.stringify(body)});
+        headers.append('X-App-Name', "mindcraft");
+        const request = new Request(url, { method, headers, body: JSON.stringify(body) });
         let data = null;
         try {
             const res = await fetch(request);
